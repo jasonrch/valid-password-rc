70a6f7818e7a812141573d0a6334fda12f6dd953
jasonrch <151870228+jasonrch@users.noreply.github.com>
2015-10-24 14:32:15 -0400

    Initial Commit

---
+++ VaildPasswordRC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VaildPasswordRC
+//
+//  Created by Julio Reyes on 10/24/15.
+//  Copyright © 2015 Julio Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VaildPasswordRC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VaildPasswordRC
+//
+//  Created by Julio Reyes on 10/24/15.
+//  Copyright © 2015 Julio Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
